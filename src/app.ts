946d9605205f6cdc634c9633a7af34d65f102eaf
andrastoth021 <andrastoth021@gmail.com>
2024-10-30 18:25:40 +0100

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,17 @@
 import fastify from 'fastify';
 import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
 import cors from '@fastify/cors';
-import { readFile, writeFile } from 'node:fs/promises';
-import { PathLike, existsSync, writeFileSync } from 'node:fs';
-
-function getNextId<T extends {id: number}>(items: T[]) {
-  if (items.length === 0) {
-    return 1;
-  }
-  const ids = items.map(item => item.id);
-  const maxId = Math.max(...ids);
-  return maxId + 1;
-}
-
-class JsonFileStore<T> {
-  constructor(private readonly path: PathLike) {
-    if(!existsSync(this.path)) {
-      writeFileSync(this.path, '[]', 'utf-8');
-    }
-  }
-
-  async read() {
-    const content = await readFile(this.path, 'utf-8');
-    const data = JSON.parse(content) as T[];
-    return data 
-  }
-  async write(data: T[]) {
-    const content = JSON.stringify(data, null, 2);
-    await writeFile(this.path, content, 'utf-8');
-  } 
-
-}
-
-
-type Pet = {
-  id: number,
-  name: string
-  food: number,
-  weight: number
-  age: number,
-}
+import { PathLike } from 'node:fs';
+import { JsonFileStore } from './utils/json-file-store';
+import { Pet } from './business/pet-type'
+import { PetService } from './business/pet-service';
 
 export default async function createApp(options = {}, dataFilePath: PathLike) {
   const app = fastify(options).withTypeProvider<JsonSchemaToTsProvider>()
   await app.register(cors, {});
 
   const petStore = new JsonFileStore<Pet>(dataFilePath);
+  const petService = new PetService(petStore);
 
   const postPetSchema = {
     body: {
@@ -61,20 +27,13 @@ export default async function createApp(options = {}, dataFilePath: PathLike) {
     '/pets',
     { schema: postPetSchema },
     async (request, reply) => {
+      // read the HTTP REQUEST parameters
       const { name } = request.body
 
-      const pets = await petStore.read();
-      const nextId = getNextId(pets);
-      const newPet: Pet = {
-        id: nextId,
-        name,
-        food: 1,
-        weight: 1,
-        age: 1
-      }
-      pets.push(newPet);
-      await petStore.write(pets);
+      // Do the business logic
+      const newPet = await petService.born(name);
 
+      // HTTP RESPONSE
       reply.status(201);
       return newPet;
     }
---
+++ src/business/pet-service.ts
@@ -0,0 +1,23 @@
+import { PetRepository } from '../data-access/pet-repository';
+import { JsonFileStore } from '../utils/json-file-store';
+import { Pet, PetProperties } from './pet-type'
+
+export class PetService {
+    private readonly repository;
+
+    constructor(store: JsonFileStore<Pet>) {
+        this.repository = new PetRepository(store);
+    }
+
+    async born(name: string) {
+        const newPet: PetProperties = {
+            name,
+            food: 1,
+            weight: 1,
+            age: 1
+        }
+
+        const created = await this.repository.create(newPet);
+        return created;
+    }
+}
